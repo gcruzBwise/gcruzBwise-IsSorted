fdf6c3a921cd0e3f572f277cbecad5f84b26f2fb
gcruzBwise <gcruz28@wgu.edu>
2023-03-09 20:44:56 +0000

    Complete

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -1,3 +1,5 @@
+import java.util.stream.IntStream;
+
 public class IsSorted {
     /**
      * Return true if arr is sorted, smallest to largest, false otherwise.
@@ -7,6 +9,6 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
-    }
+        return IntStream.range(0, arr.length - 1).noneMatch(i -> arr[i] > arr[i + 1]);
+}
 }
